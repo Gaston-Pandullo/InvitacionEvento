e271613816cf1a17caa86290d93d8a80fcae0435
Gastón Pandullo <gpandullo3@gmail.com>
2025-04-29 10:37:10 -0300

    Primer Commit

---
+++ InvitacionWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InvitacionWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
