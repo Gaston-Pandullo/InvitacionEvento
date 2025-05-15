c7c9682ed2149abb54e7d96e01017ec39e87b78e
Gaston-Pandullo <140354738+Gaston-Pandullo@users.noreply.github.com>
2025-05-15 09:28:18 -0300

    Delete InvitacionWeb directory

--- InvitacionWeb/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace InvitacionWeb.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
